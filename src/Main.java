ec5ea7592192625101a6867f58f5d172280c4152
KevinMejia <kevinm861@nycstudents.net>
2025-11-19 08:41:28 -0500

    attempting to add code to check if player 2 guesses the unscrambled code correctly or not and the number of attempts needed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner Scan = new Scanner(System.in);
-        System.out.print("Hello Player 2! ");
+        System.out.print("Hello Player 1! ");
 
         //User input for the word (userInput) to be scrambled
         System.out.print("What word would you like Player 2 to guess?: ");
@@ -27,12 +27,31 @@ public class Main {
         }
 
         //To create a gap so Player 2 won't see (or at least struggle to see) the original word.
-        for (int i = 0; i<=10; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println("");
         }
 
         System.out.println("Hello Player 2!");
         System.out.println("The scrambled word Player 1 chose for you to guess is: " + scrambledWord + "!");
+
+        //Player 2 guess
+        System.out.print("Please input your first guess as to what the word could be: ");
+        String guess = Scan.nextLine();
+
+        Boolean guessCompared = Randomizer.checkGuess(guess, userWord);
+        int attempts = 0;
+
+//        while (!guessCompared) {
+//            if (guessCompared) {
+//                attempts++;
+//                System.out.println("Player 2 guessed the word right in " + attempts + " attempts!");
+//            } else {
+//                attempts++;
+//                System.out.println("Try again!: ");
+//                guess = Scan.nextLine();
+//                guessCompared = Randomizer.checkGuess(guess, userWord);
+//            }
+//        }
 
 
     }
--- src/Randomizer.java
+++ src/Randomizer.java
@@ -13,5 +13,29 @@ public class Randomizer {
         return scrambled;
     }
 
+    //Checks if user got the unscrambled word correct
+    public static Boolean checkGuess(String playerGuess, String userWord) {
+        String lowerPlayerGuess = playerGuess.toLowerCase();
+        String lowerUserWord = userWord.toLowerCase();
+        String s = "";
 
+//        int guessLength = playerGuess.length();
+//        int wordLength = userWord.length();
+
+        for (int i = 0; i < userWord.length(); i++){
+            s += lowerUserWord.substring(0, i);
+            if (s.equals(lowerUserWord.substring(0, i)));
+        }
+        //Temporary return statement
+        return lowerPlayerGuess;
+    }
 }
+
+//Temporary code in case needed is below
+
+//public static String guessesCount(String playerGuess, String userWord) {
+//    int guessLength = playerGuess.length();
+//    int wordLength = userWord.length();
+//
+//    for ()
+//}
